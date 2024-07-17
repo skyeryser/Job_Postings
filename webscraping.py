699a7de8a320e6006542c34b3af728edfc6274f2
Eloise Ryser <eloiseskye@gmail.com>
2024-07-17 10:01:28 -0600

    Updating libraries for webscraping

---
+++ webscraping.py
@@ -0,0 +1,5 @@
+import requests #grab web-page
+from bs4 import BeautifulSoup #webscrape
+from collections import defaultdict #default dictionary: store a list with each key
+import pandas as pd #dataframe
+
